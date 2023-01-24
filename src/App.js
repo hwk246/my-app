7203b16752bb33af56dee9a91c1fd90a19dea363
Ron Klinker <r.klinker@gmail.com>
2023-01-24 14:28:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/booklist">
+              BookList
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booklist" element={<BookList />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/BookList.js
@@ -0,0 +1,5 @@
+function BookList() {
+  return <h1>BookList Page</h1>;
+}
+
+export default BookList;
