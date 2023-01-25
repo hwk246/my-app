21cddbb77abb4064b780648afd92f0d21aeeb6eb
Ron Klinker <r.klinker@gmail.com>
2023-01-25 14:52:37 +0100

    added page dotSH

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Link, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import FavoriteSites from "./components/FavoriteSites";
+import DotSH from "./components/DotSH";
 
 function App() {
   return (
@@ -18,11 +19,17 @@ function App() {
               Favorite
             </Link>
           </li>
+          <li>
+            <Link className="link" to="/dotSH">
+              DotSH
+            </Link>
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorite" element={<FavoriteSites />} />
+        <Route path="/dotSH" element={<DotSH />} />
       </Routes>
     </div>
   );
---
+++ src/components/DotSH.js
@@ -0,0 +1,7 @@
+import dotSH from "../image/dotSH.png";
+
+function DotSH() {
+  return <img src={dotSH} alt="snippet"></img>;
+}
+
+export default DotSH;
