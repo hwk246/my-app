b4c41956a0314bcf1d8f57af2274573328a12a9e
H.W  Klinker <103109647+hwk246@users.noreply.github.com>
2025-06-23 12:41:38 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="home-img">
-      <h1>Raspberry Page</h1>
+      <h1>Raspberry Home</h1>
       <img
         src="https://www.raspberrypi.com/app/uploads/2020/06/raspberrry_pi_logo.png"
         alt="raspberry"
